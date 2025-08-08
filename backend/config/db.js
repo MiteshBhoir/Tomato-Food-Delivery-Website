86c0282e6de932ddb36be8fa1a3c35f6751b19fe
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2025-08-08 23:54:37 +0530

    Added vercel.json in all folders

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://miteshbhoir585:96536379000@cluster0.spi85.mongodb.net/FOOD')
+    await mongoose.connect(process.env.MONGO_URI)
         .then(() => {
             console.log("connected to db")
         }).catch((err) => {
