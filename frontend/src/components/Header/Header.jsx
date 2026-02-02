f056faadd60a281442642186be440e45b7810441
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2026-02-03 02:55:18 +0530

    fixed header image

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import './Header.css'
-import { useNavigate } from 'react-router-dom'
+import './Header.css' 
+import headerImg from "../../assets/header_img.png";
+
 const Header = () => {
     return (
-        <div className='header' >
+        <div className='header' style={{ backgroundImage: `url(${headerImg})` }} >
             <div className="header-contents">
                 <h2> Order your favourite food here</h2>
                 <p>Choose form a diverse menu featuring a delectable array fo dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisy your cravings and elevate your dining experince ,one delicious meal at a time. </p>
